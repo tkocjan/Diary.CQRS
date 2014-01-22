6b69fea761fec06ee9f3626036c75fd6f083ec9a
Leo-Lei <leiwei2094@gmail.com>
2014-01-22 08:40:19 -0800

    c

--- src/Diary.CQRS.Web/Controllers/HomeController.cs
+++ src/Diary.CQRS.Web/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Diary.CQRS.Commands;
+using Diary.CQRS.Configuration;
+using Diary.CQRS.Reporting;
 
 namespace Diary.CQRS.Web.Controllers
 {
@@ -13,8 +16,57 @@ namespace Diary.CQRS.Web.Controllers
 
         public ActionResult Index()
         {
+            ViewBag.Model = ServiceLocator.ReportDatabase.GetItems();
             return View();
         }
+
+
+        public ActionResult Edit(Guid id)
+        {
+            var item = ServiceLocator.ReportDatabase.GetById(id);
+            var model = new DiaryItemDto()
+            {
+                Description = item.Description,
+                From = item.From,
+                Id = item.Id,
+                Title = item.Title,
+                To = item.To,
+                Version = item.Version
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(DiaryItemDto item)
+        {
+            ServiceLocator.CommandBus.Send(new ChangeItemCommand(item.Id, item.Title, item.Description, item.From, item.To, item.Version));
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(Guid id)
+        {
+            var item = ServiceLocator.ReportDatabase.GetById(id);
+            ServiceLocator.CommandBus.Send(new DeleteItemCommand(item.Id, item.Version));
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(DiaryItemDto item)
+        {
+            ServiceLocator.CommandBus.Send(new CreateItemCommand(Guid.NewGuid(), item.Title, item.Description, -1, item.From, item.To));
+
+            return RedirectToAction("Index");
+
+        }
+
+
 
     }
 }
---
+++ src/Diary.CQRS/CommandHandlers/CreateItemCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Diary.CQRS.Commands;
+using Diary.CQRS.Domain;
+using Diary.CQRS.Storage;
+
+namespace Diary.CQRS.CommandHandlers
+{
+    public class CreateItemCommandHandler:ICommandHandler<CreateItemCommand>
+    {
+        private IRepository<DiaryItem> _repository;
+
+        public CreateItemCommandHandler(IRepository<DiaryItem> repository )
+        {
+            _repository = repository;
+        }
+
+        public void Execute(CreateItemCommand command)
+        {
+            if (command==null)
+            {
+                throw new Exception();
+            }
+            if (_repository==null)
+            {
+                throw new Exception();
+            }
+            var aggregate = new DiaryItem(command.Id, command.Title, command.Description, command.From, command.To);
+            aggregate.Version = -1;
+            _repository.Save(aggregate, aggregate.Version);
+        }
+    }
+}
